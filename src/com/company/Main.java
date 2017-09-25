e51af354d58ef9ab9a6a44e51104af55aec8a406
JHqqueen <fsl1184@yahoo.com.tw>
2017-09-25 14:17:49 +0800

    vv

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("106024048_何易宗");// write your code here
+        System.out.print("test!");
+    }
+}
